34a1a910e01ef2a8c24b7e611b32bbf94b3c385a
Juha Lehtinen <jtlehtinen@fastmail.com>
2025-10-16 23:17:59 +0300

    3.14: phonebook database, step 2

--- part03/phonebook-backend/src/controllers/persons.js
+++ part03/phonebook-backend/src/controllers/persons.js
@@ -41,6 +41,33 @@ router.post('/', async (req, res) => {
   res.status(201).json(createdPerson)
 })
 
+router.put('/:id', async (req, res) => {
+  const id = req.params.id
+  const name = req.body?.name
+  const number = req.body?.number
+
+  const errors = []
+  if (!name) errors.push('name is required')
+  if (!number) errors.push('number is required')
+
+  if (errors.length > 0) {
+    res.status(400).json({ errors })
+    return
+  }
+
+  const updatedPerson = await Person.findByIdAndUpdate(
+    id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+
+  if (updatedPerson) {
+    res.json(updatedPerson)
+  } else {
+    res.status(404).end()
+  }
+})
+
 router.delete('/:id', async (req, res) => {
   const id = req.params.id
   await Person.findByIdAndDelete(id)
