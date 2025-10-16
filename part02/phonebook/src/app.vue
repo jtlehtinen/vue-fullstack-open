9526e2f9405796bb5336a235440697918fe18540
Juha Lehtinen <jtlehtinen@fastmail.com>
2025-10-16 20:37:02 +0300

    2.6: phonebook, step 1

---
+++ part02/phonebook/src/app.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { nanoid } from 'nanoid'
+import { ref } from 'vue'
+
+const name = ref('')
+
+const persons = ref([
+  { id: nanoid(), name: 'Juha Lehtinen' },
+])
+
+function handleSubmitPersonForm() {
+  const nameValue = name.value.trim()
+
+  if (!nameValue) {
+    alert('name is required')
+    return
+  }
+
+  persons.value = [
+    ...persons.value,
+    { id: nanoid(), name: nameValue },
+  ]
+
+  name.value = ''
+}
+</script>
+
+<template>
+  <main>
+    <h1>Phonebook</h1>
+
+    <form @submit.prevent="handleSubmitPersonForm">
+      <div>
+        <label>
+          Name:
+          <input
+            id="name-input"
+            v-model="name"
+          />
+        </label>
+        <button type="submit">Add</button>
+      </div>
+    </form>
+
+    <section>
+      <h2>Numbers</h2>
+      <ul>
+        <li v-for="person of persons" :key="person.id">
+          {{ person.name }}
+        </li>
+      </ul>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 720px;
+  margin: 0 auto;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+button {
+  margin-left: 0.5rem;
+}
+</style>
