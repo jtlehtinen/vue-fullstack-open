3b11de21f05e01b0208a223e9ab6802c5ddc7d6a
Juha Lehtinen <jtlehtinen@fastmail.com>
2025-10-16 20:17:17 +0300

    1.12: anecdotes, step 1

---
+++ part01/anecdotes/src/app.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+import { anecdotes } from './anecdotes'
+import { getRandomInt } from './lib/random'
+
+const selected = ref(0)
+
+function nextAnecdote() {
+  if (anecdotes.length <= 1) {
+    return
+  }
+
+  let next = selected.value
+  while (next === selected.value) {
+    next = getRandomInt(anecdotes.length)
+  }
+  selected.value = next
+}
+</script>
+
+<template>
+  <main>
+    <section>
+      <p>{{ anecdotes[selected] }}</p>
+      <button @click="nextAnecdote">Next anecdote</button>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+main {
+  max-width: 720px;
+  margin: 0 auto;
+}
+</style>
