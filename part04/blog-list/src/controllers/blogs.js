374e124497661620bdedcb9ff1966f2815208825
Juha Lehtinen <jtlehtinen@fastmail.com>
2025-10-17 11:17:00 +0300

    4.17: blog list expansion, step 5

--- part04/blog-list/src/controllers/blogs.js
+++ part04/blog-list/src/controllers/blogs.js
@@ -1,16 +1,30 @@
 import express from 'express'
-import { Blog } from '../models/index.js'
+import { Blog, User } from '../models/index.js'
 
 export const router = express.Router()
 
 router.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog
+    .find()
+    .populate('user', { username: 1, name: 1 })
+
   return response.json(blogs)
 })
 
 router.post('/', async (request, response) => {
-  const blog = new Blog(request.body)
+  // @TODO: Temporary, pick _a_ user.
+  const user = await User.findOne()
+  if (!user) {
+    response.status(400).json({ error: 'no user found' })
+    return
+  }
+
+  const blog = new Blog({ ...request.body, user: user._id })
   const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
--- part04/blog-list/src/controllers/users.js
+++ part04/blog-list/src/controllers/users.js
@@ -5,7 +5,10 @@ import { User } from '../models/index.js'
 export const router = express.Router()
 
 router.get('/', async (_request, response) => {
-  const users = await User.find({})
+  const users = await User
+    .find()
+    .populate('blogs', { title: 1, author: 1, url: 1, likes: 1 })
+
   return response.json(users)
 })
 
