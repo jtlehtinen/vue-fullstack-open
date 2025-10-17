f7c428d0d14ad23b7b30721575da0474aed93347
Juha Lehtinen <jtlehtinen@fastmail.com>
2025-10-17 14:43:55 +0300

    5.7: blog list frontend, step 7

--- part05/blog-list-frontend/src/components/blog.vue
+++ part05/blog-list-frontend/src/components/blog.vue
@@ -1,9 +1,47 @@
 <script setup>
-defineProps({
+import { ref } from 'vue'
+
+const props = defineProps({
   blog: { type: Object, required: true }
 })
+
+const showDetails = ref(false)
+
+function handleToggleDetails() {
+  showDetails.value = !showDetails.value
+}
+
+function handleLike() { }
 </script>
 
 <template>
-  <div>{{ blog.title }} {{ blog.author }}</div>
+  <div class="blog">
+    {{ blog.title }} {{ blog.author }}
+    <button @click="handleToggleDetails">
+      {{ showDetails ? 'Hide' : 'View' }}
+    </button>
+
+    <template v-if="showDetails">
+      <div>{{ blog.url }}</div>
+      <div>
+        Likes: {{ blog.likes }}
+        <button @click="handleLike">Like</button>
+      </div>
+      <div>Added by {{ blog.user.username }}</div>
+
+    </template>
+  </div>
 </template>
+
+<style scoped>
+.blog {
+  border: 1px solid black;
+  padding: 0.25rem 0.5rem;
+  margin-bottom: 0.5rem;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+</style>
